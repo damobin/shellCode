9b1a93cf6ad3f413f508ac063eeb6f24d26efe69
mo <286711902@qq.com>
2020-09-25 14:39:16 +0800

    完成gitbackup shell脚本

--- window_shell/all_git_pushback/all_gitbackup.sh
+++ window_shell/all_git_pushback/all_gitbackup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 function gitbackup_funs(){
+	if [ $# -lt 2 ];then
+		local deep=0
+	else
+		local deep=$2
+	fi
+	
 	for file in `ls $1 -a`
 	do
 		if [ $file == ".." ] || [ $file == "." ] ;then
@@ -8,16 +14,31 @@ function gitbackup_funs(){
 		
 		if [ -d $1"/"$file ]	#获取目录的每一个单元 的相对执行路径  判断是否是目录
 		then
-			gitbackup_funs $1
+		[ 0 -eq 1 ] &&{		#注释
+			#一层
+			if [ $deep -lt 2 ];then
+				echo "No directory $1"/"$file"
+				gitbackup_funs "$1"/"$file" 2
+			fi
+		}
+			#两层
+			if [ $deep -lt 2 ];then
+				echo "round path = $1"/"$file  echo deep= $deep"
+				gitbackup_funs "$1"/"$file" `expr $deep + 1`
+			fi
+			
+			
 		else
 			if [ $file == ".gitignore" ]		#对于 node_modules 目录不进行处理
 			then
+				cd $1
 				git add .
 				git commit -m "backup"
 				git push
+				cd -
 			fi
 		fi
-	done
+	done	
 }
 
 echo "git back All files yes or no?"
